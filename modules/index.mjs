78962c8cccb9906caf81f6f03089abbd07e7968d
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-03 18:40:34 +0530

    ES6 Style Import Exports

---
+++ modules/index.mjs
@@ -0,0 +1,9 @@
+import { add, sub } from "./math.mjs";
+// const math = require("./math");
+
+// import "./math.mjs";
+// import { add } from "./math";
+
+console.log(add(1, 3));
+console.log(sub(5, 3));
+// console.log("Sub : ", sub(5, 2));
---
+++ modules/math.mjs
@@ -0,0 +1,24 @@
+// function add() {
+
+// }
+
+const a = 10;
+console.log();
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// module.exports = {
+//   add: add,
+//   sub: sub,
+// };
+// exports.add = add;
+
+// export function add() {
+//   return 1;
+// }
