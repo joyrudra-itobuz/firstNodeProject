ec5b1c72c813f851ba0191b395f075a50d7d3796
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-06 11:43:23 +0530

    Importing All Modules

--- modules/index.mjs
+++ modules/index.mjs
@@ -1,9 +1,9 @@
-import { add, sub } from "./math.mjs";
+import * as joy from "./math.mjs";
 // const math = require("./math");
 
 // import "./math.mjs";
 // import { add } from "./math";
 
-console.log(add(1, 3));
-console.log(sub(5, 3));
+console.log(joy.add(1, 3));
+console.log(joy.sub(5, 3));
 // console.log("Sub : ", sub(5, 2));
