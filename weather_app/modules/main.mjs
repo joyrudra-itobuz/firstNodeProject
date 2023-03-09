eb686b82606774cc99060677033fb74a9ea7d841
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-09 18:38:00 +0530

    CRUD System

--- weather_app/modules/main.mjs
+++ weather_app/modules/main.mjs
@@ -1,4 +1,5 @@
-import * as data from "./weatherDb.mjs";
+// import * as data from "./weatherDb.mjs";
+import * as fs from "fs";
 
 //Global Declarations
 let defaultData = {
@@ -17,35 +18,91 @@ let defaultData = {
   feelslikeF: "70",
 };
 
+let weatherData = "";
+
+// fs.readFile("weatherDb.csv", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   weatherData = JSON.parse(data);
+// });
+
+console.log("Starts!");
+
+try {
+  const data = fs.readFileSync("weatherDb.csv", "utf8");
+  weatherData = JSON.parse(data);
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
+
+console.log(weatherData);
+
+// //remove
+// function removeData(location) {
+//   const result = data.weatherData.findIndex(
+//     (data) => data.location === location
+//   );
+//   data.weatherData.splice(result, 1);
+//   console.log("Removing Kolkata ", result);
+// }
+
+// removeData("Agra");
+// console.log("Calling remove :  ", data.weatherData);
+
+// //add
+// function addData(location) {
+//   defaultData.location = location;
+//   data.weatherData.push(defaultData);
+// }
+
+// addData("Agra");
+// console.log("Calling Add : ", data.weatherData);
+
+// //edit
+// function editData(location, tempC, tempF, humidity) {
+//   console.log(tempC, tempF, humidity);
+//   const findData = data.weatherData.find((data) => data.location === location);
+//   findData.tempC = tempC;
+//   findData.tempF = tempF;
+//   findData.humidity = humidity;
+// }
+
 //remove
 function removeData(location) {
-  const result = data.weatherData.findIndex(
-    (data) => data.location === location
-  );
-  data.weatherData.splice(result, 1);
+  const result = weatherData.findIndex((data) => data.location === location);
+  weatherData.splice(result, 1);
   console.log("Removing Kolkata ", result);
 }
 
 removeData("Agra");
-console.log("Calling remove :  ", data.weatherData);
+console.log("Calling remove :  ", weatherData);
 
 //add
 function addData(location) {
   defaultData.location = location;
-  data.weatherData.push(defaultData);
+  weatherData.push(defaultData);
 }
 
 addData("Agra");
-console.log("Calling Add : ", data.weatherData);
+console.log("Calling Add : ", weatherData);
 
 //edit
 function editData(location, tempC, tempF, humidity) {
   console.log(tempC, tempF, humidity);
-  const findData = data.weatherData.find((data) => data.location === location);
+  const findData = weatherData.find((data) => data.location === location);
   findData.tempC = tempC;
   findData.tempF = tempF;
   findData.humidity = humidity;
 }
 
 editData("Agra", "45", "90", "88");
-console.log("Calling Edit : ", data.weatherData);
+console.log("Calling Edit : ", weatherData);
+
+fs.writeFile("weatherDb.csv", JSON.stringify(weatherData), (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Written!");
+});
