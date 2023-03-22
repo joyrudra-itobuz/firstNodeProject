b924f5866cc320b931c720104e440f8971f30fd0
joyrudra-itobuz <joyrudra@itobuz.com>
2023-03-22 18:30:00 +0530

    Removed more comments

--- modules/index.mjs
+++ modules/index.mjs
@@ -1,9 +1,4 @@
 import * as math from "./math.mjs";
-// const math = require("./math");
-
-// import "./math.mjs";
-// import { add } from "./math";
 
 console.log(math.add(1, 3));
 console.log(math.sub(5, 3));
-// console.log("Sub : ", sub(5, 2));
--- modules/math.mjs
+++ modules/math.mjs
@@ -1,7 +1,3 @@
-// function add() {
-
-// }
-
 const a = 10;
 console.log();
 
@@ -12,13 +8,3 @@ export function add(a, b) {
 export function sub(a, b) {
   return a - b;
 }
-
-// module.exports = {
-//   add: add,
-//   sub: sub,
-// };
-// exports.add = add;
-
-// export function add() {
-//   return 1;
-// }
